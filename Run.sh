152ddfc5005b480e3a1e14381b4c5e112b5d3d0e
PeterPan <63651361+RookiePanIsLearning@users.noreply.github.com>
2024-10-14 23:59:34 +0800

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# // Login Ubuntu
+
 proot-distro login ubuntu
-# // Current Direct to Samsung Tab S9+'s code-server folder. 
+
 cd code-server-4.16.1-linux-arm64
 cd bin
-# // Open localhost:8080 with Chrome
+
 xdg-open  http://localhost:8080/login
-# // OPEN code-server
+
 export PASSWORD="Piers841228"
 ./code-server
 

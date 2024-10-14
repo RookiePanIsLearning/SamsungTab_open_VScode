9c8db1d50d0457f939d7710192fc38bb2bd229b5
PeterPan <63651361+RookiePanIsLearning@users.noreply.github.com>
2024-10-14 22:51:32 +0800

    Update Run.sh
    change code rows

--- Run.sh
+++ Run.sh
@@ -4,8 +4,10 @@
 cd code-server-4.16.1-linux-arm64
 cd bin
 
+# OPEN code-server
+./code-server
+export PASSWORD="Piers841228"
+
 # Open localhost:8080 with Chrome
 open -a http://localhost:8080/login
 
-./code-server
-export PASSWORD="Piers841228"
